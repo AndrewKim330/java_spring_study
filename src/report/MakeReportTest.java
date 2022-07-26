5237dc7f4db00ebf3fc43ba4c601026f1487b31c
AndrewKim330 <rendezvous330@gmail.com>
2022-07-26 22:28:18 +0900

    feat: Construct class for making report

---
+++ src/report/MakeReportTest.java
@@ -0,0 +1,11 @@
+package report;
+
+public class MakeReportTest {
+
+    public static void main (String [] args) {
+        MakeReport builder = new MakeReport();
+        String report = builder.getReport();
+
+        System.out.println(report);
+    }
+}
