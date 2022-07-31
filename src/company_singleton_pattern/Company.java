757a1f3d564c169ec4110f7de48684926f4eae8b
AndrewKim330 <rendezvous330@gmail.com>
2022-07-31 16:57:41 +0900

    feat: Construct class for company(singleton pattern)

---
+++ src/company_singleton_pattern/Company.java
@@ -0,0 +1,17 @@
+package company_singleton_pattern;
+
+public class Company {
+
+    private static Company instance = new Company();
+
+    private Company() {
+
+    }
+
+    public static Company getInstance() {
+        if (instance == null) {
+            instance = new Company();
+        }
+        return instance;
+    }
+}
