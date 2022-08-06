50e5e3e9fe0eddad8d90ff051ab21c3827559986
AndrewKim330 <rendezvous330@gmail.com>
2022-08-06 16:49:28 +0900

    feat: Construct class for making score management program

---
+++ src/scoreManagement/Subject.java
@@ -0,0 +1,20 @@
+package scoreManagement;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Subject {
+
+    private String subjectName;
+    private int subjectScore;
+    ArrayList<Integer> subjectList = new ArrayList<>();
+
+    public Array getSubject() {
+
+        return
+    }
+
+    public void setSubjectList(ArrayList<Integer> subjectList) {
+        this.subjectList = subjectList;
+    }
+}
