ed9224a8f5f060adea2ee77a2d24eff5bfd08608
AndrewKim330 <rendezvous330@gmail.com>
2022-08-11 23:43:28 +0900

    feat: Construct class for making membership management program with polymorphism

--- src/membershipInheritance/CustomerTest.java
+++ src/membershipInheritance/CustomerTest.java
@@ -17,9 +17,5 @@ public class CustomerTest {
 
         // upCasting
         Customer vc = new VIPCustomer(12345,"noName");
-<<<<<<< HEAD
-        vc.
-=======
->>>>>>> ce3b314 (feat: Construct class for making membership management program with class inheritance)
     }
 }
