ca8d2bb04c53df9aa37318af9f1c6349533ada7c
AndrewKim330 <rendezvous330@gmail.com>
2022-08-22 23:23:25 +0900

    feat: Basic of Spring framework - Object Mapper

--- src/test/java/com/example/hellospring/HelloSpringApplicationTests.java
+++ src/test/java/com/example/hellospring/HelloSpringApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.hellospring;
 
+import com.example.hellospring.dto.User2;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HelloSpringApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        System.out.println("------------");
+
+        var objectMapper = new ObjectMapper();
+
+        var user = new User2("andrew", 35, "010-1111-2222");
+
+        var text = objectMapper.writeValueAsString(user);
+        System.out.println(text);
+
+        var objectUser = objectMapper.readValue(text, User2.class);
+        System.out.println(objectUser);
+
     }
 
 }
